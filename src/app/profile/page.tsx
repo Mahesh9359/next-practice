88f79878b61e6cd475c86dfee7e6d09c65635f64
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-07-30 19:26:02 +0530

    access Profile details from provider login

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,24 +1,22 @@
-'use client';
+"use client";
 
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import userAvatar from "public/icons/person.png";
+import Image from "next/image";
 
 export default function ProfilePage() {
-//   const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
-  const [quizHistory, setQuizHistory] = useState<any[]>([]);
 
   useEffect(() => {
-    if (status === 'unauthenticated') {
-      router.push('/login');
-    } else {
-      const data = localStorage.getItem('quizHistory');
-      setQuizHistory(data ? JSON.parse(data) : []);
+    if (status === "unauthenticated") {
+      router.push("/login");
     }
   }, [status]);
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return <div className="text-center mt-10">Loading...</div>;
   }
 
@@ -27,31 +25,31 @@ export default function ProfilePage() {
       <h1 className="text-3xl font-bold mb-6">Profile</h1>
 
       <div className="bg-white shadow rounded-lg p-6 flex items-center space-x-4 mb-8">
-        <img
-        //   src={session?.user?.image || '/default-avatar.png'}
-          alt="User Avatar"
+        <Image
+          src={session?.user?.image || userAvatar}
+          alt="User Image"
+          width={64}
+          height={64}
           className="w-16 h-16 rounded-full"
         />
         <div>
-          <h2 className="text-xl font-semibold">M</h2>
-          <p className="text-gray-600">M</p>
+          <h2 className="text-xl font-semibold">{session?.user?.name}</h2>
+          <p className="text-gray-600">{session?.user?.email}</p>
         </div>
       </div>
 
       <h2 className="text-2xl font-semibold mb-4">Quiz History</h2>
 
-      {quizHistory.length === 0 ? (
-        <p className="text-gray-500">No quiz attempts yet.</p>
-      ) : (
-        <div className="space-y-4">
-          {quizHistory.map((quiz, index) => (
-            <div key={index} className="bg-gray-100 p-4 rounded-md shadow">
-              <p><strong>Score:</strong> {quiz.score}</p>
-              <p><strong>Date:</strong> {new Date(quiz.date).toLocaleString()}</p>
-            </div>
-          ))}
+      <div className="space-y-4">
+        <div className="bg-gray-100 p-4 rounded-md shadow">
+          <p>
+            <strong>Score:</strong>{" "}
+          </p>
+          <p>
+            <strong>Date:</strong>{" "}
+          </p>
         </div>
-      )}
+      </div>
     </div>
   );
 }
